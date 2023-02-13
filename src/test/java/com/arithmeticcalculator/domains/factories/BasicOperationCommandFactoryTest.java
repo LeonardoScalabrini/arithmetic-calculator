6956324f3654e78f3f47a813009222cb7fbc4ca0
Leonardo <leonardo_scalabrini@hotmail.com>
2023-02-13 03:10:16 -0300

    Formatted code

--- src/test/java/com/arithmeticcalculator/domains/factories/BasicOperationCommandFactoryTest.java
+++ src/test/java/com/arithmeticcalculator/domains/factories/BasicOperationCommandFactoryTest.java
@@ -19,8 +19,8 @@ class BasicOperationCommandFactoryTest {
     assertEquals(-1.0, BasicOperationCommandFactory.of(SUBTRACTION, 1, 2).execute());
     assertEquals(1.0, BasicOperationCommandFactory.of(DIVISION, 1, 1).execute());
     assertEquals(0.0, BasicOperationCommandFactory.of(DIVISION, 0, 1).execute());
-    assertThrows(
-        OperationException.class, () -> BasicOperationCommandFactory.of(DIVISION, 1, 0).execute());
+    var division = BasicOperationCommandFactory.of(DIVISION, 1, 0);
+    assertThrows(OperationException.class, division::execute);
     assertEquals(0.5, BasicOperationCommandFactory.of(DIVISION, 2, 4).execute());
     assertEquals(1.0, BasicOperationCommandFactory.of(MULTIPLICATION, 1, 1).execute());
     assertEquals(0.25, BasicOperationCommandFactory.of(MULTIPLICATION, 0.5, 0.5).execute());
